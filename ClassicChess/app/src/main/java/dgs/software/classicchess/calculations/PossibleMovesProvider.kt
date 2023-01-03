d3ca498b248ad4070dbf2e469894fce43de2afa8
Sebastian Müller <seb.mueller.91@web.de>
2023-01-03 11:37:52 +0100

    Added move calculations for some pieces

--- ClassicChess/app/src/main/java/dgs/software/classicchess/calculations/PossibleMovesProvider.kt
+++ ClassicChess/app/src/main/java/dgs/software/classicchess/calculations/PossibleMovesProvider.kt
@@ -15,39 +15,63 @@ private val TAG = "PossibleMovesProvider"
 class PossibleMovesProvider(
     val game: Game
 ) {
-    fun getUnfilteredMoves(piece: Piece): List<Coordinate> {
+    fun getUnfilteredMoves(position: Coordinate): List<RevertableMove> {
+        if (game.get(position) is Cell.Empty) {
+            Log.e(TAG, "Tried to calculate moves for an empty cell at pos $position")
+            return listOf<RevertableMove>()
+        }
+        val piece = game.getAsPiece(position)
         return when (piece.type) {
-            Type.PAWN -> piece.getUnfilteredMovesForPawn()
-            Type.ROOK -> piece.getUnfilteredMovesForRook()
-            Type.KNIGHT -> piece.getUnfilteredMovesForKnight()
-            Type.BISHOP -> piece.getUnfilteredMovesForBishop()
-            Type.KING -> piece.getUnfilteredMovesForKing()
-            Type.QUEEN -> piece.getUnfilteredMovesForQueen()
+            Type.PAWN -> piece.getUnfilteredMovesForPawn(position)
+            Type.ROOK -> piece.getUnfilteredMovesForRook(position)
+            Type.KNIGHT -> piece.getUnfilteredMovesForKnight(position)
+            Type.BISHOP -> piece.getUnfilteredMovesForBishop(position)
+            Type.KING -> piece.getUnfilteredMovesForKing(position)
+            Type.QUEEN -> piece.getUnfilteredMovesForQueen(position)
         }
     }
 
-    fun Piece.getUnfilteredMovesForPawn(): List<Coordinate> {
+    fun Piece.getUnfilteredMovesForPawn(position: Coordinate): List<RevertableMove> {
         throw java.lang.Exception("Not implemented")
     }
 
-    private fun Piece.getUnfilteredMovesForQueen(): List<Coordinate> {
+    private fun Piece.getUnfilteredMovesForQueen(position: Coordinate): List<RevertableMove> {
+        var possibleMoves = mutableListOf<RevertableMove>()
+        possibleMoves.addCellsOnStraightLines(position)
+        possibleMoves.addCellsOnDiagonalLines(position)
+        possibleMoves.addNeighborCells(position)
+        return possibleMoves
+    }
+
+    private fun Piece.getUnfilteredMovesForKing(position: Coordinate): List<RevertableMove> {
         throw java.lang.Exception("Not implemented")
     }
 
-    private fun Piece.getUnfilteredMovesForKing(): List<Coordinate> {
-        throw java.lang.Exception("Not implemented")
+    private fun Piece.getUnfilteredMovesForBishop(position: Coordinate): List<RevertableMove> {
+        var possibleMoves = mutableListOf<RevertableMove>()
+        possibleMoves.addCellsOnDiagonalLines(position)
+        return possibleMoves
     }
 
-    private fun Piece.getUnfilteredMovesForBishop(): List<Coordinate> {
-        throw java.lang.Exception("Not implemented")
+    private fun Piece.getUnfilteredMovesForKnight(position: Coordinate): List<RevertableMove> {
+        var possibleMoves = mutableListOf<RevertableMove>()
+
+        possibleMoves.addMoveIfValid(position, position.copy(position.row+2,position.column+1))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row+2,position.column-1))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row-2,position.column+1))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row-2,position.column-1))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row+1,position.column+2))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row-1,position.column+2))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row+1,position.column-2))
+        possibleMoves.addMoveIfValid(position, position.copy(position.row-1,position.column-2))
+
+        return possibleMoves
     }
 
-    private fun Piece.getUnfilteredMovesForKnight(): List<Coordinate> {
-        throw java.lang.Exception("Not implemented")
-    }
-
-    private fun Piece.getUnfilteredMovesForRook(): List<Coordinate> {
-        throw java.lang.Exception("Not implemented")
+    private fun Piece.getUnfilteredMovesForRook(position: Coordinate): List<RevertableMove> {
+        var possibleMoves = mutableListOf<RevertableMove>()
+        possibleMoves.addCellsOnStraightLines(position)
+        return possibleMoves
     }
 
     private fun MutableList<RevertableMove>.addNeighborCells(fromPos: Coordinate) {
@@ -121,8 +145,7 @@ class PossibleMovesProvider(
 
     private fun MutableList<RevertableMove>.addMoveAndCheckIfToContinue(
         fromPos: Coordinate,
-        toPos: Coordinate,
-        conditionToAdd: (Coordinate) -> Boolean = { true }
+        toPos: Coordinate
     ): Boolean {
         if (!(game.get(toPos) is Cell.Empty)
             || game.getAsPiece(fromPos).player == game.getAsPiece(toPos).player
@@ -130,7 +153,7 @@ class PossibleMovesProvider(
             return false
         }
         if (!addMoveIfValid(fromPos, toPos)) {
-            return false // TODO: Make sure if it should stop in all cases, especially for duplicated moves
+            return false
         }
         return game.get(toPos) is Cell.Empty
     }
